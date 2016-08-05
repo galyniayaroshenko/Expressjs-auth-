f49169719fc79bc10f61da25a20f20085936f308
yaroshenko.g <yaroshenko270@gmail.com>
2016-08-05 17:06:30 +0300

    finish auth

--- app/controllers/api/index.js
+++ app/controllers/api/index.js
@@ -2,10 +2,8 @@ import express from 'express';
 
 import users from './users';
 import auth from './auth';
-import performer from './performer';
 let api = express.Router();
 
 api.use('/users', users);
 api.use('/auth', auth);
-api.use('/performer', performer);
 export default api;
